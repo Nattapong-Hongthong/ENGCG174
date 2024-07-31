8b1694fef0f518a585908aced9fbfb204b9965d1
Nattapong-Hongthong <108038990+Nattapong-Hongthong@users.noreply.github.com>
2024-08-01 05:02:31 +0700

    Add files via upload

---
+++ LAB4/10.py
@@ -0,0 +1,15 @@
+set1 = [1, 2, 3, 4, 5]
+set2 = [3, 4, 5, 6, 7]
+
+union_set = set1 | set2
+print("Union set:", union_set) 
+
+intersection_set = set1 & set2
+print("Intersection set:", intersection_set) 
+
+difference_set = set1 - set2
+print("Difference set (set1 - set2):", difference_set) # Outp
+(1, 2)
+
+symmetric_difference_set = set1 ^ set2
+print("Symmetric difference set:", symmetric_difference_set)
---
+++ LAB4/3.py
@@ -0,0 +1,5 @@
+numbers = [5, 2, 8, 1, 3]
+numbers.sort()
+print("Sorted list:", numders)
+numbers.reverse()
+print("Reversed list", numbers)
